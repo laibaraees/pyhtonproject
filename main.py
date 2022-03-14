3286979d1aaad9d5f27f5bd7562e7ad09f9ff4f1
laibaraees <98691061+laibaraees@users.noreply.github.com>
2022-03-14 19:22:31 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Import Libraries
+from deepface import DeepFace
+import matplotlib.pyplot as plt
+import cv2 as cv
+import os
+
+#Read or Load data
+img=cv.imread('img1.jpg')
+plt.imshow(img[:,:,::-1])
+
+result=DeepFace.analyze(img)
+
+plt.imshow(img[:,:,::-1])
+plt.title("2k19-BSCS-306" + "\n" "Hello" +result["gender"])
+
+#image Analyzer
+print("Emotions:" ,result)
+print("Angry:",+result['emotion']['angry'])
+print("Disgust:",+result['emotion']['disgust'])
+print("Fear:",+result['emotion']['fear'])
+print("Happy:",+result['emotion']['happy'])
+print("Sad:",+result['emotion']['sad'])
+print("Surprise:",+result['emotion']['surprise'])
+print("Neutral:",+result['emotion']['neutral'])
+print("Age:",+result['age'])
+
+plt.show()
